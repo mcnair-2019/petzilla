d5aaf636100fbc4aad01c046d7efe657e23a0caa
ktapia576 <ktapia576@hotmail.com>
2019-07-18 16:12:10 -0400

    Use path.join

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const port = 3000;
 
 const app = express();
 
-app.use(express.static(__dirname + '/views'));      // Sets routes for html files in "/views"
-app.use(express.static(__dirname + '/public'));     // Sets routes for Files in "/public", mainly for CSS
+app.use(express.static(path.join(__dirname, '/views')));      // Sets Static Folder routes for html files in "/views"
+app.use(express.static(path.join(__dirname, '/public')));     // Sets Static Folder routes for Files in "/public", mainly for CSS
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}!`)
