52d9bc0ba0a4f5af11b6feb076a757a7dd604bbf
ktapia576 <ktapia576@hotmail.com>
2019-07-18 04:00:39 -0400

    Add Express Routing

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');     
 const path = require('path');
 const hbs = require('hbs');     // Load Handlebars
+const port = 3000;
 
 const app = express();
 
-console.log(__dirname);
+app.use(express.static(__dirname + '/views'));      // Sets routes for html files in "/views"
+app.use(express.static(__dirname + '/public'));     // Sets routes for Files in "/public", mainly for CSS
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}!`)
+});
