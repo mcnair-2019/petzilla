c4d02b02ba82a69fc3689d7f0ebab4e9342b204f
ktapia576 <ktapia576@hotmail.com>
2019-07-21 18:44:02 -0400

    Formatted Code for easier readability

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');     
 const path = require('path');
 const hbs = require('hbs');     // Load Handlebars
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/views')));      // Sets Static Folder routes for html files in "/views"
 app.use(express.static(path.join(__dirname, '/public')));     // Sets Static Folder routes for Files in "/public", mainly for CSS
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}!`)
-});
+app.listen(PORT, () => console.log(`Server listening on port ${port}!`));
