fe5329b43c61f11b74e676b6529a73949609de30
ktapia576 <ktapia576@hotmail.com>
2019-07-21 19:07:40 -0400

    Add handlebars functionality and setup routes for server

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-const express = require('express');     
+const express = require('express');
+const app = express();   
 const path = require('path');
-const hbs = require('hbs');     // Load Handlebars
+const exphbs  = require('express-handlebars'); 
+
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+app.use(express.static(path.join(__dirname, '/public')));     // Sets Static Folder routes for Files in "/public", mainly for CSS and Images
 
-app.use(express.static(path.join(__dirname, '/views')));      // Sets Static Folder routes for html files in "/views"
-app.use(express.static(path.join(__dirname, '/public')));     // Sets Static Folder routes for Files in "/public", mainly for CSS
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
-app.listen(PORT, () => console.log(`Server listening on port ${port}!`));
+// Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Home page route
+app.get('/', (req, res) => {
+    res.render('index', {layout: 'index.handlebars'}); // Render "/views/index.handlebars" and use the layout "/views/layouts/index.handlebars"
+});
+
+// Cat page route
+app.get('/cats', (req, res) => {
+    res.send('This will be cat slideshow!');
+});
+
+// Dog page route
+app.get('/dogs', (req, res) => {
+    res.send('This will be dog slideshow!');
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
