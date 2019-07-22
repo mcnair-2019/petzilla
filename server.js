37205c652dc8946cb90b016ca99799b72039c1eb
ktapia576 <ktapia576@hotmail.com>
2019-07-21 22:09:59 -0400

    Add routing and handlebars template

--- server.js
+++ server.js
@@ -12,22 +12,36 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 // Handlebars Middleware, exphbs() is to modify config/options for hbs
-app.engine('handlebars', exphbs({ defaultLayout: 'main', layoutsDir: path.join(__dirname, '/views/layouts')}));
+app.engine('handlebars', exphbs({ 
+    defaultLayout: 'main', 
+    layoutsDir: path.join(__dirname, '/views/layouts')
+}));
 app.set('view engine', 'handlebars');
 
 // Home page route
-app.get('/', (req, res) => {
-    res.render('index', {layout: 'index.handlebars'}); // Render "/views/index.handlebars" and use the layout "/views/layouts/index.handlebars"
+app.get('/', (req, res) => {    
+    res.render('index', {       // Render "/views/index.handlebars" and use the layout "/views/layouts/index.handlebars"
+        title: 'Home Page',
+        css: '<link rel="stylesheet" type="text/css" href="css/home.css" >'
+    }); 
 });
 
 // Cat page route
 app.get('/cats', (req, res) => {
-    res.send('This will be cat slideshow!');
+    res.render('gallery', {
+        title: 'Cats!',
+        css: '<link rel="stylesheet" type="text/css" href="css/gallery.css" >',
+        script: '<script src="scripts/gallery./js"></script>'
+    });
 });
 
 // Dog page route
 app.get('/dogs', (req, res) => {
-    res.send('This will be dog slideshow!');
+    res.render('gallery', {
+        title: 'Dogs!',
+        css: '<link rel="stylesheet" type="text/css" href="css/gallery.css" >',
+        script: '<script src="scripts/gallery./js"></script>'
+    });
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
