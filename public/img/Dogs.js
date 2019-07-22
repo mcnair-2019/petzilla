3ae6918b0855b3b06b4a0d84192e02dc49337f05
ktapia576 <ktapia576@hotmail.com>
2019-07-22 15:39:06 -0400

    Add json for img paths

---
+++ public/img/Dogs.js
@@ -0,0 +1,20 @@
+// Add an atribute called data-title for image captions
+const dogs = [
+    {
+        img: 'img/id4_dog_pic1.jpeg'
+    },
+    {
+        img: 'img/id5_dog_pic1.jpeg'
+    },
+    {
+        img: 'img/id6_dog_pic1.JPG'
+    },
+    {
+        img: 'img/id8_dog_pic1.png'
+    },
+    {
+        img: 'img/id8_dog_post1.png'
+    }
+];
+
+module.exports = dogs;
