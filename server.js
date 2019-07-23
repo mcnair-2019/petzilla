1fc6033023deddf0c68ad7bf17a5c07c623c520b
ktapia576 <ktapia576@hotmail.com>
2019-07-22 23:48:04 -0400

    Add Cat API functionality

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();   
 const path = require('path');
 const exphbs  = require('express-handlebars'); 
+const request = require('request');
+const dbconfig = require('./dbconfig.json');    // Load dbconfig file with API key
 
 const dogs = require('./public/img/Dogs');  //Get Dog paths
 const cats = require('./public/img/Cats');  //Get Cats paths
@@ -31,11 +33,37 @@ app.get('/', (req, res) => {
 
 // Cat page route
 app.get('/cats', (req, res) => {
-    res.render('gallery', {
-        title: 'Cats!',
-        css: '<link rel="stylesheet" type="text/css" href="css/gallery.css" >',
-        script: '<script src="scripts/gallery.js"></script>',
-        imgs: cats
+    var catJSON;    // Cat API json data
+    var cats = [];  // Cat URLs array
+
+    var options = {     // options for cat API
+        method: 'GET',
+        url: 'https://api.thecatapi.com/v1/images/search?limit=5',  // limit=5 returns 5 images
+        headers: {
+            'x-api-key': dbconfig["api-key"]
+        }
+    };
+    
+    request(options, (err, response, body) => {      // Send get request to cat API. Note: Use response instead of res, will override previous res variable. 
+        if(err){ 
+            throw new Error(err);
+        }
+
+        catJSON = JSON.parse(body);     // Parse JSON formatted String into JSON
+
+        for(var i in catJSON) {     // Push url into array
+            var item = catJSON[i];  // Store single cat objectin item
+            cats.push(item.url);    // Push cat object's url 
+        }
+
+        // Must call res.render() inside request() due to callback based approach.
+        // (cats array will not be filled by the time res.render() gets called)
+        res.render('gallery', {     
+            title: 'Cats!',         
+            css: '<link rel="stylesheet" type="text/css" href="css/gallery.css" >',
+            script: '<script src="scripts/gallery.js"></script>',
+            imgs: cats
+        });
     });
 });
 
